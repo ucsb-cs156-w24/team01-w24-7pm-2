21dd71ada6ce8fbe5cbb4676edd7c768fb420698
Michael Wong <mpwong@ucsb.edu>
2024-01-25 16:40:15 -0800

    mw - wrote test for TIdesQueryServiceTests

---
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryServiceTests.java
@@ -0,0 +1,40 @@
+package edu.ucsb.cs156.spring.backenddemo.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
+
+@RestClientTest(TidesQueryService.class)
+public class TidesQueryServiceTests {
+    @Autowired
+    private MockRestServiceServer mockRestServiceServer;
+
+    @Autowired
+    private TidesQueryService tidesQueryService;
+
+    @Test
+    public void test_getJSON() { //beginDate, endDate, station
+        String beginDate = "feb";
+        String endDate = "mar";
+        String station = "IV";
+        String expectedUrl = TidesQueryService.ENDPOINT.replace("{beginDate}", beginDate).replace("{endDate}", endDate).replace("{station}", station);
+
+        String fakeJsonResult = "{\"fake\" : \"result\" }";
+
+        this.mockRestServiceServer.expect(requestTo(expectedUrl))
+            .andExpect(header("Accept", MediaType.APPLICATION_JSON.toString()))
+            .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
+            .andRespond(withSuccess(fakeJsonResult, MediaType.APPLICATION_JSON));    
+    
+        String actualResult = tidesQueryService.getJSON(beginDate, endDate, station);
+        assertEquals(fakeJsonResult, actualResult);
+        }
+    
+}

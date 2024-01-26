af1475a3318b327fc29a5e55f78cd805be1cc6fe
Andy Gale <agale@ucsb.edu>
2024-01-25 19:47:15 -0800

    ag - added service to get information on zip codes

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/ZipCodeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/ZipCodeQueryService.java
@@ -1,14 +1,27 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-import org.springframework.web.client.RestTemplate;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
-
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ZipCodeQueryService {
+
+    ObjectMapper mapper = new ObjectMapper();
 
     private final RestTemplate restTemplate;
 
@@ -18,7 +31,23 @@ public class ZipCodeQueryService {
 
     public static final String ENDPOINT = "http://api.zippopotam.us/us/{zipcode}";
 
-    public String getJSON(String zipcode) throws HttpClientErrorException {
-       return "";
+    public String getJSON(String postcode, String country, String countryabbreviation, String places)
+            throws HttpClientErrorException {
+        log.info("post code={}, country={}, country abbreviation={}, places={}", postcode, country, countryabbreviation,
+                places);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        Map<String, String> uriVariables = Map.of("post code", postcode, "country", country, "country abbreviation",
+                countryabbreviation,
+                "places", places);
+
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
+                uriVariables);
+        return re.getBody();
     }
+
 }

cf5d1d2f17ddfa1530c302ecb37f710c6b5ce850
Kavya Verma <kavyaverma@ucsb.edu>
2024-01-30 15:59:04 -0800

    kv - fixed location controller and tests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
@@ -4,17 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import edu.ucsb.cs156.spring.backenddemo.services.LocationQueryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 
 @Tag(name="Locations from https://nominatim.openstreetmap.org/")
 @RestController
@@ -26,10 +26,12 @@ public class LocationController {
     @Autowired
     LocationQueryService locationQueryService;
 
-    @Operation(summary = "Get a location")
+    @Operation(summary = "Get a list of locations", description = "JSON return format documented here: https://nominatim.openstreetmap.org/")
     @GetMapping("/get")
-    public ResponseEntity<String> getLocation() throws JsonProcessingException {
-        String result = locationQueryService.getJSON("Isla Vista");
+    public ResponseEntity<String> getLocation(
+        @Parameter(name="location", description="location", example="Isla Vista") @RequestParam String location
+    ) throws JsonProcessingException {
+        String result = locationQueryService.getJSON(location);
         return ResponseEntity.ok().body(result);
     }
 
--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,17 +11,27 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import edu.ucsb.cs156.spring.backenddemo.services.LocationQueryService;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 
 
 @WebMvcTest(value = LocationController.class)
 public class LocationControllerTests {
+  private ObjectMapper mapper = new ObjectMapper();
   @Autowired
   private MockMvc mockMvc;
   @MockBean
@@ -29,9 +41,10 @@ public class LocationControllerTests {
   public void test_getLocation() throws Exception {
   
     String fakeJsonResult="{ \"fake\" : \"result\" }";
-    when(mockLocationQueryService.getJSON("Isla Vista")).thenReturn(fakeJsonResult);
+    String location = "Isla Vista";
+    when(mockLocationQueryService.getJSON(eq(location))).thenReturn(fakeJsonResult);
 
-    String url = String.format("/api/locations/get");
+    String url = String.format("/api/locations/get?location=%s",location);
 
     MvcResult response = mockMvc
         .perform( get(url).contentType("application/json"))
